cddb46598aafc22b35b63845c301d9af4cc64102
Vladislav Semenov <arctic.ratatat@gmail.com>
2021-12-02 17:15:56 +0300

    last commit

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -5,12 +5,43 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SQRServiceTest {
+
     @Test
-    public void shouldSqrValue() {
+    public void shouldSqrValueTwoInRange() {
         SQRService service = new SQRService();
         int min = 350;
         int max = 400;
         int expected = 2;
+        int actual = service.sqrValue(min, max);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSqrValueMinOneInRange() {
+        SQRService service = new SQRService();
+        int min = 450;
+        int max = 500;
+        int expected = 1;
+        int actual = service.sqrValue(min, max);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSqrValueMinNotInRange() {
+        SQRService service = new SQRService();
+        int min = 790;
+        int max = 840;
+        int expected = 0;
+        int actual = service.sqrValue(min, max);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSqrValueMinFiveInRange() {
+        SQRService service = new SQRService();
+        int min = 100;
+        int max = 200;
+        int expected = 5;
         int actual = service.sqrValue(min, max);
         assertEquals(expected, actual);
     }
